d2c0e9ddb8cecfda52af6017273ef200c7d5f0a8
¨guisfons¨ <guilhermesfonsecaa@gmail.com>
2024-02-02 10:48:02 -0300

    Project ignite

---
+++ contact-management.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Contact Management Plugin
+ * Plugin URI: https://github.com/guisfons/alfasoft-plugin
+ * Description: Plugin dedicated to the contact management.
+ * Version: 1.0
+ * Author: Guilherme Silva Fonseca
+ * Author URI: https://github.com/guisfons
+ */
