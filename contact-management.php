b51d8d5b486d276fa62fe872d151bae68db57ea5
guisfons <guilhermesfonsecaa@gmail.com>
2024-02-02 16:43:22 -0300

    Correction of code and implemention of delete function

--- contact-management.php
+++ contact-management.php
@@ -12,25 +12,35 @@
 
 global $wpdb;
 $tablePeople = $wpdb->prefix . 'people';
+$tableContact = $wpdb->prefix . 'contact';
 
 function cm_init() {
     if(is_admin()) {
         add_action('admin_menu', 'cm_add_menu_pages');
 
-        global $wpdb, $tablePeople;
+        global $wpdb, $tablePeople, $tableContact;
     
         $charset_collate = $wpdb->get_charset_collate();
     
-        $sql = "CREATE TABLE $tablePeople (
+        $sqlPeople = "CREATE TABLE $tablePeople (
             id mediumint(9) NOT NULL AUTO_INCREMENT,
             name varchar(255) NOT NULL,
             email varchar(255) NOT NULL UNIQUE,
             PRIMARY KEY (id)
         ) $charset_collate;";
-    
-        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 
-        dbDelta($sql);
+        $sqlContact = "CREATE TABLE $tableContact (
+            id mediumint(9) NOT NULL AUTO_INCREMENT,
+            personId int NOT NULL,
+            countryCode varchar(7) NOT NULL,
+            number varchar(9) NOT NULL,
+            PRIMARY KEY (id),
+            FOREIGN KEY (personId) REFERENCES $tablePeople(id),
+        ) $charset_collate;";
+        
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        
+        dbDelta($sqlPeople, $sqlContact);
     }
 
     // Public part
@@ -97,10 +107,28 @@ function cm_admin_page() {
                 <tbody>';
 
                 foreach ($results as $result) {
-                    echo "<tr><td>{$result->id}</td><td>{$result->name}</td><td>{$result->email}</td></tr>";
+                    echo
+                    '<tr>
+                        <td>'.$result->id.'</td>
+                        <td>'.$result->name.'</td>
+                        <td>'.$result->email.'</td>
+                        <td>
+                            <form method="post" action="">
+                                <input type="hidden" name="contact_person_delete" value="'.$result->id.'">
+                                <input type="submit" name="submit_delete" value="X">
+                            </form>
+                        </td>
+                    </tr>';
                 }
 
                 echo '</tbody>';
+
+                if (isset($_POST['submit_delete'])) {
+                    global $wpdb;
+                    $tablePeople = $wpdb->prefix . 'people';
+                    $resultId = $_POST['contact_person_delete'];
+                    $wpdb->delete( $tablePeople, array( 'id' => $resultId ) );
+                }
             ?>
         </table>
     </div>
@@ -112,17 +140,24 @@ function cm_add_new_person_page() {
     
 
     if (isset($_POST['submit_person'])) {
-        // Process form data and add a new person
         $name = sanitize_text_field($_POST['contact_name']);
         $email = sanitize_email($_POST['contact_email']);
 
-        // Validate input before adding to the database
         if (!empty($name) && filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            global $wpdb;
             $tablePeople = $wpdb->prefix . 'people';
 
-            insert($tablePeople, ['name' => $name, 'email' => $email]);
+            $query = "SELECT * FROM $tablePeople WHERE email = '$email'";
 
-            echo '<div class="contact-management__updated"><p>New person added successfully!</p></div>';
+            $results = $wpdb->get_results($query);
+
+            if(!empty($results)) {
+                echo '<div class="contact-management__error"><p>This email is already in use.</p></div>';
+            } else {
+                insert($tablePeople, ['name' => $name, 'email' => $email]);
+                echo '<div class="contact-management__updated"><p>New person added successfully!</p></div>';
+            }
+
         } else {
             echo '<div class="contact-management__error"><p>Please provide valid name and email.</p></div>';
         }
@@ -143,12 +178,66 @@ function cm_add_new_person_page() {
 }
 
 function cm_add_new_contact_page() {
+    global $wpdb;
+
+    if (isset($_POST['submit_contact'])) {
+        $tel = $_POST['contact_tel'];
+
+        if (!empty($tel)) {
+            $tableContact = $wpdb->prefix . 'contact';
+
+            insert($tableContact, ['tel' => $tel, 'email' => $email]);
+
+            echo '<div class="contact-management__updated"><p>New person added successfully!</p></div>';
+        } else {
+            echo '<div class="contact-management__error"><p>Please provide valid telephone and email.</p></div>';
+        }
+    }
     ?>
-    <div class="wrap">
-        <h1>List Contacts</h1>
-        <table class="widefat">
-        </table>
-    </div>
+    <section class="contact-management__add">
+        <h1>Add New Contact</h1>
+
+        <form method="post" action="">
+            <label for="contact_country_code">Country Code:
+                <select name="contact_person" id="">
+                    <option value="" selected disabled>Select Person Email</option>
+                    <?php
+                    global $wpdb, $tablePeople;
+
+                    $query = "SELECT * FROM $tablePeople";
+
+                    $results = $wpdb->get_results($query);
+
+                    foreach ($results as $result) {
+                        echo "<option value=".$result->email.">{$result->email}</option>";
+                    }
+                    ?>
+                </select>
+                <select name="contact_country_code" id="" required>
+
+                    <?php
+                        $apiEndpoint = 'https://restcountries.com/v3.1/all';
+                        $response = file_get_contents($apiEndpoint);
+                        $data = json_decode($response, true);
+                        
+                        $countryData = array_map(function ($country) {
+                            $countryName = $country['name']['common'];
+                            $countryCode = isset($country['idd']['root']) ? $country['idd']['root'].$country['idd']['suffixes'][0] : 'N/A';
+                            
+                            return array('name' => $countryName, 'code' => $countryCode);
+                        }, $data);
+
+
+                        foreach ($countryData as $countryInfo) {
+                            echo '<option value="'.$countryInfo['code'].'">'.$countryInfo['name'].' ('.$countryInfo['code'].')</option>';
+                        }
+                    ?>
+                </select>
+            </label>
+            <label for="contact_tel">Telephone: <input type="tel" name="contact_tel" minlength="9" maxlength="9"></label>
+            <input type="submit" name="submit_contact" value="Add Contact">
+        </form>
+    </section>
     <?php
 }
 
