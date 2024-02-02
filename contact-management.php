b237db5043da4840ccb5c982dffc00f350e2ef6e
guisfons <guilhermesfonsecaa@gmail.com>
2024-02-02 11:38:02 -0300

    structure for the management area

--- contact-management.php
+++ contact-management.php
@@ -9,24 +9,15 @@
  */
 
 
-add_action('admin_menu', 'cm_plugin_menu');
+add_action('admin_menu', 'cm_init');
 
-function cm_plugin_menu() {
+function cm_init() {
     add_menu_page(
         'Contact Management',
         'Contact Management',
         'manage_options',
         'contact-management-plugin',
         'cm_admin_page'
-    );
-
-    add_submenu_page(
-        'contact-management-plugin',
-        'List People',
-        'List People',
-        'manage_options',
-        'list-people',
-        'cm_list_people_page'
     );
 
     add_submenu_page(
@@ -49,17 +40,34 @@ function cm_plugin_menu() {
 }
 
 function cm_admin_page() {
-    
-}
+    ?>
+    <div class="wrap">
+        <h1>Contact Management</h1>
+        <p>Welcome to the Contact Management plugin!</p>
 
-function cm_list_people_page() {
+        <hr>
 
+        <h2>List People</h2>
+        <table class="widefat">
+        </table>
+    </div>
+    <?php
 }
 
 function cm_add_new_person_page() {
-
+    ?>
+    <div class="wrap">
+        <h1>Add New Person</h1>
+    </div>
+    <?php
 }
 
 function cm_add_new_contact_page() {
-
+    ?>
+    <div class="wrap">
+        <h1>List Contacts</h1>
+        <table class="widefat">
+        </table>
+    </div>
+    <?php
 }
